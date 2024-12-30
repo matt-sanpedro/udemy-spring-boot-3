5a5b752869e176fd400c9f5ffe6754737f6acbd4
matt-sanpedro <mattsanpedro7@gmail.com>
2024-12-29 18:34:38 -0800

    Created new project with spring web and spring boot devtools

---
+++ 05-spring-boot-spring-core/src/main/java/com/luv2code/spingcoredemo/SpingcoredemoApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.spingcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingcoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingcoredemoApplication.class, args);
+	}
+
+}
