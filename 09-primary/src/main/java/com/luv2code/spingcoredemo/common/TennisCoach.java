73bcaa98422c1aa451e5d636659b18e336d30cae
matt-sanpedro <mattsanpedro7@gmail.com>
2025-06-01 13:41:47 -0700

    Initialized project, removed @Qualifier annotation and added @Primary annotation to the tennis coach class

---
+++ 09-primary/src/main/java/com/luv2code/spingcoredemo/common/TennisCoach.java
@@ -0,0 +1,15 @@
+package com.luv2code.spingcoredemo.common;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary // This annotation indicates that this class is a primary bean for dependency injection
+public class TennisCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your backhand volley for 30 minutes!";
+    }
+
+}
---
+++ 09-primary/src/test/java/com/luv2code/spingcoredemo/SpingcoredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.spingcoredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingcoredemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
