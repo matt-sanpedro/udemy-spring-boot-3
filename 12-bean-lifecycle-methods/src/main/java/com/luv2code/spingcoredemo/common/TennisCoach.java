c6a680cab7a42962ce91c9835b469b4ebe967b9b
matt-sanpedro <mattsanpedro7@gmail.com>
2025-06-01 23:15:18 -0700

    Implemented init and destroy method

--- 12-bean-lifecycle-methods/src/main/java/com/luv2code/spingcoredemo/common/TennisCoach.java
+++ 12-bean-lifecycle-methods/src/main/java/com/luv2code/spingcoredemo/common/TennisCoach.java
@@ -2,6 +2,9 @@ package com.luv2code.spingcoredemo.common;
 
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Component
 // @Primary // This annotation indicates that this class is a primary bean for dependency injection
 public class TennisCoach implements Coach {
@@ -9,6 +12,18 @@ public class TennisCoach implements Coach {
     // add a no argument constructor
     public TennisCoach() {
         System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    // define init method
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("In doMyStartupStuff() for: " + getClass().getSimpleName());
+    }
+
+    // define detroy method
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("In doMyCleanupStuff() for: " + getClass().getSimpleName());
     }
 
     @Override
