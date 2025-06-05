2d7cf36846de2200c342a3b1df256861d89c9937
matt-sanpedro <mattsanpedro7@gmail.com>
2025-06-04 22:39:42 -0700

    Initialized new project and removed init (@PostConstruct) and destroy (@PreDestroy) methods

---
+++ section-00-spring-core/13-java-config-bean/src/main/java/com/luv2code/spingcoredemo/common/TennisCoach.java
@@ -0,0 +1,22 @@
+package com.luv2code.spingcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
+@Component
+// @Primary // This annotation indicates that this class is a primary bean for dependency injection
+public class TennisCoach implements Coach {
+
+    // add a no argument constructor
+    public TennisCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your backhand volley for 30 minutes!";
+    }
+
+}
